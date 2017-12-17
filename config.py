5c9ae0f29b40e2278a686e12196b983a6e1e0e17
avagut <lgathua@gmail.com>
2017-12-17 16:35:11 +0300

    Add instance folder and config file

---
+++ config.py
@@ -0,0 +1,14 @@
+DEBUG = True
+SECRET_KEY = 'this is my secret'
+SQLALCHEMY_DATABASE_URI = "sqlite:///flask_dance.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+WTF_CSRF_ENABLED = True
+
+GOOGLE_ID = "google client"
+GOOGLE_SECRET = "google secret"
+
+GITHUB_ID = 'github id'
+GITHUB_SECRET = 'github secret'
+
+TWITTER_API_KEY = 'twitter api key'
+TWITTER_API_SECRET = 'twitter api secret'
